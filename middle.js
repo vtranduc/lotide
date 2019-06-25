a7b9ddcec2f99ee78e7ac69b84c02f5607407be6
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-06-25 18:32:51 +0000

    updated

--- middle.js
+++ middle.js
@@ -2,13 +2,13 @@ const assertArraysEqual = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     //return false;
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2} expected`);
-    return
+    return;
   } else {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         //return false;
         console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2} expected`);
-        return
+        return;
       }
     }
     //return true;
@@ -31,22 +31,22 @@ const eqArrays = function(arr1, arr2) {
 
 const middle = function(arr) {
   if (arr.length === 1 || arr.length === 2) {
-    return [];  
+    return [];
   } else {
     if (arr.length % 2 === 1) {
       return arr[(arr.length - 1) / 2];
     } else if (arr.length % 2 === 0) {
       const firstIndex = arr.length / 2 - 1;
-      return arr.slice(firstIndex, firstIndex + 2)
+      return arr.slice(firstIndex, firstIndex + 2);
     }
   }
 };
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
