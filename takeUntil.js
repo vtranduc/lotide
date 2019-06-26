03d8cab560c601405433d844da85bc4555f18fd2
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-06-26 22:43:11 +0000

    updated

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,21 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    //return false;
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2} expected`);
+    return
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        //return false;
+        console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2} expected`);
+        return
+      }
+    }
+    //return true;
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2} expected`);
+  }
+};
+
 const takeUntil = function(array, callback) {
   // ...
   let output = [];
@@ -21,3 +39,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
